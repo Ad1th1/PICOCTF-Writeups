16f2f21f9b707be38a457b0765d3dc5e3bfd289a
Ad1th1 <adithibhatindia@gmail.com>
2022-06-30 16:00:48 +0530

    Create The Numbers.py

---
+++ Cryptography/The Numbers.py
@@ -0,0 +1,6 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+lis = [20,8,5,14,21,13,2,5,18,19,13,1,19,15,14]
+for i in lis:
+    print(alphabet[i-1], end ="")
+    
+ # Or on second thought, I could have used chr and ord
